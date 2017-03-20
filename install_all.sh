792513cc40f213e4c139042cd6f6800a5d93e756
Vitaly <vtmilyakov@yandex.ru>
2017-03-20 19:19:27 +0300

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -21,6 +21,7 @@ COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tag
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+sudo usermod -aG docker $USER
 
 # instal pygment for cat
 sudo apt -y install python-pygments
